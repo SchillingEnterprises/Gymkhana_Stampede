8c2c99ee29333dc7b7108f84de7e4e359d6858ee
Gavin Schilling <schillgc@gmail.com>
2018-06-05 12:30:34 -0400

    Add User, Checkpoint, Event, & Reward models

--- KDF/Derby_Event/models.py
+++ KDF/Derby_Event/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from geoposition.fields import GeopositionField
 
-# Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email_address = models.EmailField()
+    birthday = models.DateField()
+    location = GeopositionField()
+    proof = models.ImageField()
+
+
+class Checkpoint(models.Model):
+    location = GeopositionField()
+    photograph = models.ImageField()
+    information = models.TextField()
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=50)
+    time = models.DateTimeField()
+    location = GeopositionField()
+    point_value = models.IntegerField()
+
+
+class Reward(models.Model):
+    point_total = models.IntegerField()
